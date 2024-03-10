1a0f66e305561156e5c342edb7eb11068d4afbf5
Dylan <34380246+DKotzer@users.noreply.github.com>
2024-03-10 16:45:34 -0400

    csrf fix

--- poecollection/settings.py
+++ poecollection/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY'),
 DEBUG = True
 
 ALLOWED_HOSTS = ['exile.dylankotzer.com', '192.168.2.207', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['exile.dylankotzer.com']
+CSRF_TRUSTED_ORIGINS = ['https://exile.dylankotzer.com']
 
 
 # Application definition
